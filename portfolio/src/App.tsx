c28ac5ef1391de4a4d3953cba04b3afeb58c2d13
valvanogabryel <valvanogabryel@gmail.com>
2023-04-18 12:19:33 -0300

    ;

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,10 +1,14 @@
+import Atropos from 'atropos/react';
+import 'atropos/atropos.css';
+
 const App = () => {
   return (
-    <div className="flex justify-center items-center bg-slate-600 w-screen h-screen">
-      <h1 className="font-black text-center text-white p-4 duration-300 border rounded-xl cursor-pointer hover:text-slate-600 hover:bg-white">Olá, Portifas</h1>
-      <span className="absolute p-8 w-1/12 rounded-full hover:animate-ping hover:bg-white hover:opacity-5 cursor-pointer"></span>
+    <div>
+      <Atropos className='h-10 w-10'>
+
+      </Atropos>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
