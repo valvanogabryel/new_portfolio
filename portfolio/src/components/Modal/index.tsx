2a325f946dd9b002fd2bc62703fff609ce28cea1
valvanogabryel <valvanogabryel@gmail.com>
2023-04-20 14:23:45 -0300

    Update index.tsx

--- portfolio/src/components/Modal/index.tsx
+++ portfolio/src/components/Modal/index.tsx
@@ -21,7 +21,10 @@ const Modal = ({ isOpen, onClose, project }: ModalProps) => (
       <div className="fixed bg-black bg-opacity-50 z-[1000] top-0 left-0 right-0 bottom-0">
 
         <div className="flex flex-col bg-dark_gray relative select-none">
-          <div className="absolute -right-2 -top-4 bg-slate-400 rounded-full p-3 z-50 cursor-pointer text-2xl duration-300 group">
+          <div
+            className="absolute -right-2 -top-4 bg-slate-400 rounded-full p-3 z-50 cursor-pointer text-2xl duration-300 group"
+            onClick={onClose}
+          >
             <span className="bg-slate-500 fixed -right-2 -top-4 -z-50 rounded-full p-6 cursor-pointer group-hover:animate-ping"></span>
             <AiOutlineClose className="z-50" />
           </div>
@@ -60,14 +63,20 @@ const Modal = ({ isOpen, onClose, project }: ModalProps) => (
           </div>
 
           <div>
-            <ul className='flex gap-2 text-3xl'>
-              <li className='text-light_gray duration-300 transition-all hover:scale-110 py-4 hover:text-gray-400'>
-                <a href={project.deploy}>
+            <ul className='flex gap-2 text-3xl py-4 pl-4'>
+              <li className='text-light_gray duration-300 transition-all hover:scale-110  hover:text-gray-400'>
+                <a
+                  href={project.deploy}
+                  target='_blank'
+                >
                   <AiOutlineRocket />
                 </a>
               </li>
-              <li className='text-light_gray duration-300 transition-all hover:scale-110 py-4 hover:text-gray-400'>
-                <a href={project.repository}>
+              <li className='text-light_gray duration-300 transition-all hover:scale-110 hover:text-gray-400'>
+                <a
+                  href={project.repository}
+                  target='_blank'
+                >
                   <AiFillGithub />
                 </a>
               </li>
