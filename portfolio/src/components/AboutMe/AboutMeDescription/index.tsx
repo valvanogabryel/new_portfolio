4a7cf4c700dcf40ee6dc726689abb56eaad68c7b
valvanogabryel <valvanogabryel@gmail.com>
2023-04-22 22:21:11 -0300

    ç

---
+++ portfolio/src/components/AboutMe/AboutMeDescription/index.tsx
@@ -0,0 +1,48 @@
+import Typing from 'react-typing-effect';
+import aboutMeTexts from '../texts.json';
+
+interface AboutMeDescriptionProps {
+  selected: string;
+
+}
+
+const AboutMeDescription = ({ selected }: AboutMeDescriptionProps) => {
+  return (
+    <>
+      {
+        selected === 'resume' &&
+        <Typing
+          text={aboutMeTexts.resume}
+          speed={50}
+          typingDelay={0}
+          eraseSpeed={20}
+          className=""
+        />
+      }
+      {
+        selected === 'paragraph' &&
+        <Typing
+          text={aboutMeTexts.paragraph}
+          speed={50}
+          typingDelay={0}
+          eraseSpeed={20}
+          className=""
+        />
+      }
+      {
+        selected === 'biography' &&
+        <Typing
+          text={aboutMeTexts.biography}
+          speed={50}
+          typingDelay={0}
+          eraseSpeed={20}
+          className=""
+        />
+      }
+
+    </>
+
+  );
+}
+
+export default AboutMeDescription;
