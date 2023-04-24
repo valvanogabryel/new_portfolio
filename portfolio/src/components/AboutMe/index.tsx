0a8dcb2c92eb03798e4e79fd8ba60e31ed27ca70
valvanogabryel <valvanogabryel@gmail.com>
2023-04-24 18:49:06 -0300

    ;

--- portfolio/src/components/AboutMe/index.tsx
+++ portfolio/src/components/AboutMe/index.tsx
@@ -5,6 +5,7 @@ import AnimatedBorder from "../AnimatedBorder";
 
 import AboutMeDescription from "./AboutMeDescription";
 import AboutCard from "./AboutCard";
+import { Bounce, Fade } from "react-awesome-reveal";
 
 const AboutMe = () => {
   const [selectedOption, setSelectedOption] = useState<string>('resume');
@@ -19,57 +20,66 @@ const AboutMe = () => {
 
       <div className="flex flex-col items-center">
 
+
         <div className="py-10 after:h-[1px] after:w-screen after:max-w-full after:bg-white after:bg-opacity-5 after:inline-block">
-          <label
-            htmlFor="resume"
-            className={`flex gap-2 text-light_gray px-10 relative overflow-hidden duration-500 mx-auto my-4 w-11/12 ${selectedOption === 'resume' ? 'py-4 text-white bg-background_color_darker' : null}`}
+          <Fade
+            direction="right"
+            triggerOnce
+            cascade
+            duration={1000}
           >
-            {selectedOption.match('resume') && <AnimatedBorder />}
-            <input
-              type="radio"
-              value='resume'
-              checked={selectedOption === 'resume'}
-              onChange={handleAboutMe}
-              name="resume"
-              id="resume"
-              className="scale-90 accent-primary"
-            />
-            <span>quero um resumo sobre você.</span>
-          </label>
 
-          <label
-            htmlFor="paragraph"
-            className={`flex gap-2 text-light_gray px-10 relative overflow-hidden duration-500 mx-auto my-4 w-11/12 ${selectedOption === 'paragraph' ? 'py-4 text-white bg-background_color_darker' : null}`}
-          >
-            {selectedOption.match('paragraph') && <AnimatedBorder />}
-            <input
-              type="radio"
-              value='paragraph'
-              checked={selectedOption === 'paragraph'}
-              onChange={handleAboutMe}
-              name="paragraph"
-              id="paragraph"
-              className="scale-90 accent-primary"
-            />
-            <span>me conte um pouco mais sobre você.</span>
-          </label>
+            <label
+              htmlFor="resume"
+              className={`flex gap-2 text-light_gray px-10 relative overflow-hidden duration-500 mx-auto my-4 w-11/12 ${selectedOption === 'resume' ? 'py-4 text-white bg-background_color_darker' : null}`}
+            >
+              {selectedOption.match('resume') && <AnimatedBorder />}
+              <input
+                type="radio"
+                value='resume'
+                checked={selectedOption === 'resume'}
+                onChange={handleAboutMe}
+                name="resume"
+                id="resume"
+                className="scale-90 accent-primary"
+              />
+              <span>quero um resumo sobre você.</span>
+            </label>
 
-          <label
-            htmlFor="biography"
-            className={`flex gap-2 text-light_gray px-10 relative overflow-hidden duration-500 mx-auto my-4 w-11/12 ${selectedOption === 'biography' ? 'py-4 text-white bg-background_color_darker' : null}`}
-          >
-            {selectedOption.match('biography') && <AnimatedBorder />}
-            <input
-              type="radio"
-              value='biography'
-              checked={selectedOption === 'biography'}
-              onChange={handleAboutMe}
-              name="biography"
-              id="biography"
-              className="scale-90 accent-primary"
-            />
-            <span>me conte bastante sobre você.</span>
-          </label>
+            <label
+              htmlFor="paragraph"
+              className={`flex gap-2 text-light_gray px-10 relative overflow-hidden duration-500 mx-auto my-4 w-11/12 ${selectedOption === 'paragraph' ? 'py-4 text-white bg-background_color_darker' : null}`}
+            >
+              {selectedOption.match('paragraph') && <AnimatedBorder />}
+              <input
+                type="radio"
+                value='paragraph'
+                checked={selectedOption === 'paragraph'}
+                onChange={handleAboutMe}
+                name="paragraph"
+                id="paragraph"
+                className="scale-90 accent-primary"
+              />
+              <span>me conte um pouco mais sobre você.</span>
+            </label>
+
+            <label
+              htmlFor="biography"
+              className={`flex gap-2 text-light_gray px-10 relative overflow-hidden duration-500 mx-auto my-4 w-11/12 ${selectedOption === 'biography' ? 'py-4 text-white bg-background_color_darker' : null}`}
+            >
+              {selectedOption.match('biography') && <AnimatedBorder />}
+              <input
+                type="radio"
+                value='biography'
+                checked={selectedOption === 'biography'}
+                onChange={handleAboutMe}
+                name="biography"
+                id="biography"
+                className="scale-90 accent-primary"
+              />
+              <span>me conte bastante sobre você.</span>
+            </label>
+          </Fade>
         </div>
 
         <div>
--- portfolio/src/components/Technologies/index.tsx
+++ portfolio/src/components/Technologies/index.tsx
@@ -1,6 +1,7 @@
 import Slider from 'react-slick';
 import useTechnologies from '../../hooks/useTechnologies';
 import InvisibleArrows from '../InvisibleArrows';
+import { Fade } from 'react-awesome-reveal';
 
 const Technologies = () => {
   const settings = {
@@ -32,36 +33,47 @@ const Technologies = () => {
 
   return (
     <section className='text-white text-center my-10 mt-40 border-b-[1px] border-b-slate-300 border-opacity-20 h-96'>
-      <h2 className='flex flex-grow items-center gap-8 before:h-[1px] before:w-1/6 before:bg-primary before:flex before:flex-grow after:h-[1px] after:w-1/6 after:flex after:bg-primary after:flex-grow text-2xl text-neutral-300'>Minhas Skills</h2>
-      <ul className=''>
-        <Slider
-          {...settings}
-          className='m-auto p-4'
-        >
-          {
-            techs.map(tech => (
-              <li
-                key={tech.id}
-                className=''
-              >
-                <div className={`text-[100px] relative left-[5.9rem] mt-4 mb-2 ${tech.className}`}>
-                  {tech.badge}
-                </div>
-                <div className='flex flex-col'>
-                  <span className='text-sm font-main-simple text-light_gray font-bold'>
-                    {tech.name}
-                  </span>
+      <Fade
+        direction='left'
+        triggerOnce
+      >
+        <h2 className='flex flex-grow items-center gap-8 before:h-[1px] before:w-1/6 before:bg-primary before:flex before:flex-grow after:h-[1px] after:w-1/6 after:flex after:bg-primary after:flex-grow text-2xl text-neutral-300'>
+          Minhas Skills
+        </h2>
+      </Fade>
+      <Fade
+        direction='up'
+        triggerOnce
+      >
+        <ul>
+          <Slider
+            {...settings}
+            className='m-auto p-4'
+          >
+            {
+              techs.map(tech => (
+                <li
+                  key={tech.id}
+                  className=''
+                >
+                  <div className={`text-[100px] relative left-[5.9rem] mt-4 mb-2 ${tech.className}`}>
+                    {tech.badge}
+                  </div>
+                  <div className='flex flex-col'>
+                    <span className='text-sm font-main-simple text-light_gray font-bold'>
+                      {tech.name}
+                    </span>
 
-                  <span className='text-xs text-jutify font-secondary-simple text-gray-500 text-opacity-80 mt-4 font-bold'>
-                    {tech.description}
-                  </span>
-                </div>
-              </li>
-            ))
-          }
-
-        </Slider>
-      </ul>
+                    <span className='text-xs text-jutify font-secondary-simple text-gray-500 text-opacity-80 mt-4 font-bold'>
+                      {tech.description}
+                    </span>
+                  </div>
+                </li>
+              ))
+            }
+          </Slider>
+        </ul>
+      </Fade>
     </section>
   );
 }
