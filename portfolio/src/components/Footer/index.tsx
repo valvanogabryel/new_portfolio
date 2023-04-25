a24bde0bd1986b5678eaee9789ce9e179a6eaa20
valvanogabryel <valvanogabryel@gmail.com>
2023-04-25 12:43:36 -0300

    ;

---
+++ portfolio/src/components/Footer/index.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className="">
+      <div>
+        <ul className="">
+          <li className="">linkedin</li>
+          <li className="">github</li>
+          <li className="">codepen</li>
+          <li className="">twitter</li>
+        </ul>
+      </div>
+      <div>
+        projetado e construído por <a href="" target="_blank">Gabryel Valvano.</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
