9f8035ebdcfe303babf962c431f4dd80c08200df
valvanogabryel <valvanogabryel@gmail.com>
2023-05-04 12:03:56 -0300

    Update useTraining.tsx

--- portfolio/src/hooks/useTraining.tsx
+++ portfolio/src/hooks/useTraining.tsx
@@ -1,13 +1,15 @@
 import { ITraining } from "../interfaces/ITraining";
 
 const useTraining = () => {
+  const vitrineDevLink: string = 'https://cursos.alura.com.br/vitrinedev/valvanogabryel';
+
   const training: ITraining[] = [
     {
       id: 1,
       course: 'Formação React com JavaScript',
       duration: 65,
       platform: 'Alura',
-      link: 'https://cursos.alura.com.br/vitrinedev/valvanogabryel',
+      link: vitrineDevLink,
       certificate: 'https://cursos.alura.com.br/degree/certificate/a17c9bc7-431a-4bab-b603-4cf28f7e8761',
     },
     {
@@ -21,7 +23,7 @@ const useTraining = () => {
       course: 'Formação TypeScript',
       duration: 31,
       platform: 'Alura',
-      link: 'https://cursos.alura.com.br/vitrinedev/valvanogabryel',
+      link: vitrineDevLink,
       certificate: 'https://cursos.alura.com.br/degree/certificate/50344db1-d874-4d6a-a66b-6170d8898310',
     },
     {
@@ -29,7 +31,7 @@ const useTraining = () => {
       course: 'Formação React com TypeScript',
       duration: 83,
       platform: 'Alura',
-      link: 'https://cursos.alura.com.br/vitrinedev/valvanogabryel',
+      link: vitrineDevLink,
       certificate: 'https://cursos.alura.com.br/degree/certificate/3bdfc078-79ed-4743-bbf4-4b43bb2e6ee3',
     },
     {
@@ -37,7 +39,7 @@ const useTraining = () => {
       course: 'React: Testando seus componentes',
       duration: 10,
       platform: 'Alura',
-      link: 'https://cursos.alura.com.br/vitrinedev/valvanogabryel',
+      link: vitrineDevLink,
       certificate: 'https://cursos.alura.com.br/certificate/3352658b-b281-492a-849e-632ac03dc839',
     },
     {
@@ -45,7 +47,7 @@ const useTraining = () => {
       course: 'Formação JavaScript para Front-end',
       duration: 60,
       platform: 'Alura',
-      link: 'https://cursos.alura.com.br/vitrinedev/valvanogabryel',
+      link: vitrineDevLink,
       certificate: 'https://cursos.alura.com.br/degree/certificate/7d9b096e-c2fa-4659-a939-a71ff592f4e3',
     },
     {
@@ -53,7 +55,7 @@ const useTraining = () => {
       course: 'Formação HTML & CSS',
       duration: 65,
       platform: 'Alura',
-      link: 'https://cursos.alura.com.br/vitrinedev/valvanogabryel',
+      link: vitrineDevLink,
       certificate: 'https://cursos.alura.com.br/degree/certificate/828839e6-6c50-4198-bec7-a18bd52cf4f0',
     },
     {
@@ -61,9 +63,17 @@ const useTraining = () => {
       course: 'HTTP: Entendendo a web por baixo dos panos',
       duration: 14,
       platform: 'Alura',
-      link: 'https://cursos.alura.com.br/vitrinedev/valvanogabryel',
+      link: vitrineDevLink,
       certificate: 'https://cursos.alura.com.br/certificate/b3d7e3f2-46b8-4a5a-8c7a-94d51daa9933',
-    }
+    },
+    {
+      id: 9,
+      course: 'Impulsione sua aplicação React e consuma APIs',
+      duration: 42,
+      platform: 'Alura',
+      link: vitrineDevLink,
+      certificate: 'https://cursos.alura.com.br/degree/certificate/21945220-5d3a-4dc0-a215-9b8d5879fea7',
+    },
   ];
 
   return training;
