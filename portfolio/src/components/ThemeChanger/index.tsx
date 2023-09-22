7fd2080577b20ea895af871488ad8f10d2865d94
gabryel <valvanogabryel@gmail.com>
2023-09-22 10:09:12 -0300

    Revert "Update index.tsx"
    This reverts commit 64ea62dbbeb10967f1643ba4befa4c331f08128b.

--- portfolio/src/components/ThemeChanger/index.tsx
+++ portfolio/src/components/ThemeChanger/index.tsx
@@ -4,6 +4,7 @@ import { useTheme } from "../../hooks/useTheme";
 import useScrollDirection from "../../hooks/useScrollDirection";
 
 import { motion } from "framer-motion";
+import { Fade } from "react-awesome-reveal";
 
 const ThemeChanger = () => {
   const { theme, setTheme } = useTheme();
