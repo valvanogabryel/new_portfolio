bd0cd618915a4685a7887647e72d207c317a2963
gabryel <valvanogabryel@gmail.com>
2023-09-22 10:11:17 -0300

    Revert "Update useScrollDirection.tsx"
    This reverts commit f64e5e14c5bfc378d85e6552b8e94e3ce84c0b15.

--- portfolio/src/hooks/useScrollDirection.tsx
+++ portfolio/src/hooks/useScrollDirection.tsx
@@ -1,30 +1,28 @@
 import { useEffect, useState } from "react";
 
 function useScrollDirection() {
-  const [scrollDir, setScrollDir] = useState<"down" | "up" | null>(null);
+  const [scrollDir, setScrollDir] = useState<'down' | 'up' | null>(null);
 
   useEffect(() => {
-    let lastScrollY = window.scrollY;
+    let lastScrollY = window.pageYOffset;
 
     const updateScrollDir = () => {
-      const scrollY = window.scrollY;
-      const direction = scrollY > lastScrollY ? "down" : "up";
+      const scrollY = window.pageYOffset;
+      const direction = scrollY > lastScrollY ? 'down' : 'up';
 
-      if (
-        direction !== scrollDir &&
-        (scrollY - lastScrollY > 0.5 || scrollY - lastScrollY < -0.5)
-      ) {
+      if (direction !== scrollDir && (scrollY - lastScrollY > 0.5 || scrollY - lastScrollY < -0.5)) {
         setScrollDir(direction);
       }
 
       lastScrollY = scrollY > 0 ? scrollY : 0;
-    };
+    }
 
-    window.addEventListener("scroll", updateScrollDir);
+    window.addEventListener('scroll', updateScrollDir);
 
     return () => {
-      window.removeEventListener("scroll", updateScrollDir);
-    };
+      window.removeEventListener('scroll', updateScrollDir);
+    }
+
   }, [scrollDir]);
 
   return scrollDir;
