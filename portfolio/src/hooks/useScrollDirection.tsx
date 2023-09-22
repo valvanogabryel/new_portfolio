173541744d65c978072f0eba20b8d47620ddffcd
gabryel <valvanogabryel@gmail.com>
2023-09-22 10:15:43 -0300

    fixed header

--- portfolio/src/hooks/useScrollDirection.tsx
+++ portfolio/src/hooks/useScrollDirection.tsx
@@ -1,28 +1,30 @@
 import { useEffect, useState } from "react";
 
 function useScrollDirection() {
-  const [scrollDir, setScrollDir] = useState<'down' | 'up' | null>(null);
+  const [scrollDir, setScrollDir] = useState<"down" | "up" | null>(null);
 
   useEffect(() => {
-    let lastScrollY = window.pageYOffset;
+    let lastScrollY = window.scrollY;
 
     const updateScrollDir = () => {
-      const scrollY = window.pageYOffset;
-      const direction = scrollY > lastScrollY ? 'down' : 'up';
+      const scrollY = window.scrollY;
+      const direction = scrollY > lastScrollY ? "down" : "up";
 
-      if (direction !== scrollDir && (scrollY - lastScrollY > 0.5 || scrollY - lastScrollY < -0.5)) {
+      if (
+        direction !== scrollDir &&
+        (scrollY - lastScrollY > 0.5 || scrollY - lastScrollY < -0.5)
+      ) {
         setScrollDir(direction);
       }
 
       lastScrollY = scrollY > 0 ? scrollY : 0;
-    }
+    };
 
-    window.addEventListener('scroll', updateScrollDir);
+    window.addEventListener("scroll", updateScrollDir);
 
     return () => {
-      window.removeEventListener('scroll', updateScrollDir);
-    }
-
+      window.removeEventListener("scroll", updateScrollDir);
+    };
   }, [scrollDir]);
 
   return scrollDir;
